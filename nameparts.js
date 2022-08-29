9cfd6f90d7864602760a3208cf7104a8a44ac115
Rei Sikk <reix0010@stud.kea.dk>
2022-08-29 16:46:09 +0200

    nameparts exercise done

---
+++ nameparts.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const nameStr = "REI MYSTERIO JOHNSON";
+const firstName = nameStr.substring(nameStr.indexOf(""), nameStr.indexOf(" "));
+const middleName = nameStr.substring(nameStr.indexOf(" "), nameStr.lastIndexOf(" "));
+const lastName = nameStr.substring(nameStr.lastIndexOf(" ") + 1);
+console.log(firstName.trim());
+console.log(middleName.trim());
+console.log(lastName.trim());
+const length = nameStr.length;
+/* console.log(length); */
