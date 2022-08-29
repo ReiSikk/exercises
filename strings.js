df82279182bb9c965d94e9d839a9430ac2b13460
Rei Sikk <reix0010@stud.kea.dk>
2022-08-29 11:35:53 +0200

    strings exercises

---
+++ strings.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/* const name = "Rei";
+const catname = "Mandu";
+const animaltype = "cat";
+
+console.log(`My name is ${name}.\nI have a ${animaltype} called ${catname}`);
+const len = name.length;
+console.log(`Name is: ${len} characters long`);
+
+const letter = name[2];
+console.log(letter); */
+/* const total = theName.length;
+const char = theName[2];
+console.log(`total number of characters is: ${total}`);
+console.log(`character at index 2 is ${char}`);
+const firstD = theName.search(/D/);
+console.log(firstD);
+ */
+/* const str1 = " there    is spcae here \n here";
+const str2 = str1.trim();
+console.log(`_${str2}_`); */
+
+const theName = "Albus Percival Wulfric Brian Dumbledore";
+/* const fullName = "Peter Heronimus Lind";
+const lastName = fullName.substring(16);
+console.log(`_last name is ${lastName}`); */
+
+const albus = theName.substring(0, 5);
+console.log(albus);
+const dumbl = theName.substring(29);
+const wulfric = theName.substring(15, 22);
+console.log(dumbl);
+console.log(` Wulfric `);
