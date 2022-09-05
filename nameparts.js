a8b22b6e3675e535f528cd61ce96e5fc8108c738
Rei Sikk <reix0010@stud.kea.dk>
2022-09-05 14:45:18 +0200

    nameparts done

--- nameparts.js
+++ nameparts.js
@@ -1,9 +1,20 @@
 "use strict";
 
-const nameStr = "REI MYSTERIO JOHNSON";
-const firstName = nameStr.substring(nameStr.indexOf(""), nameStr.indexOf(" "));
-const middleName = nameStr.substring(nameStr.indexOf(" "), nameStr.lastIndexOf(" "));
-const lastName = nameStr.substring(nameStr.lastIndexOf(" ") + 1);
-console.log(firstName.trim());
-console.log(middleName.trim());
-console.log(lastName.trim());
+getnameParts();
+function getnameParts(nameStr) {
+  nameStr = "REI JOHNSON";
+  const firstName = nameStr.substring(nameStr.indexOf(""), nameStr.indexOf(" "));
+  const middleName = nameStr.substring(nameStr.indexOf(" "), nameStr.lastIndexOf(" "));
+  const lastName = nameStr.substring(nameStr.lastIndexOf(" ") + 1);
+  firstName.trim();
+  middleName.trim();
+  lastName.trim();
+  if (middleName === "") {
+    middleName == null;
+    console.log("hello");
+  }
+  /*  console.log(firstName.trim());
+  console.log(middleName.trim());
+  console.log(lastName.trim()); */
+  console.log({ firstName, middleName, lastName });
+}
