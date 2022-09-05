696bc1a84e3376ccfe59c31039510e203cb419c4
Rei Sikk <reix0010@stud.kea.dk>
2022-09-05 16:18:20 +0200

    hex to rgb converter done

--- colorconvert.js
+++ colorconvert.js
@@ -2,7 +2,9 @@
 
 const cssStr = "rgb(123, 12, 1)";
 
-converter();
+/* converter(); */
+
+//CSS to rgb values
 function converter() {
   const redStr = cssStr.substring(cssStr.indexOf("(") + 1, cssStr.indexOf(","));
   const greenStr = cssStr.substring(cssStr.indexOf(" "), cssStr.lastIndexOf(" ") - 1);
@@ -15,3 +17,24 @@ function converter() {
   const blueNr = parseInt(blueStr);
   console.log({ redNr, greenNr, blueNr });
 }
+
+///hex to rgb values
+const hexStr = "#c0ffee";
+hexToRgb();
+function hexToRgb() {
+  const redPart = hexStr.substring(1, 3);
+  const greenPart = hexStr.substring(3, 5);
+  const bluePart = hexStr.substring(5, 7);
+  // making strings into numbers
+  const redNumber = parseInt(redPart, 16);
+  const greenNumber = parseInt(greenPart, 16);
+  const blueNumber = parseInt(bluePart, 16);
+  console.log(redPart);
+  console.log(greenPart);
+  console.log(bluePart);
+  console.log(redNumber);
+  console.log(greenNumber);
+  console.log(blueNumber);
+}
+
+// rgb to hex
