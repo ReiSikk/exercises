a5959a254e80aed15ab5618bb4ed5a76ed7dcdc6
Rei Sikk <reix0010@stud.kea.dk>
2022-09-06 13:31:00 +0200

    return exercises

---
+++ return.js
@@ -0,0 +1,19 @@
+let theNumber = 2;
+
+function multiplyBySelf(number) {
+  return number * number;
+}
+theNumber = multiplyBySelf(42);
+/* console.log("the nu,mber is:", theNumber); */
+
+///experiment 1 from the slides
+function greeting(firstName) {
+  return `Yo ${firstName}`;
+}
+const result = greeting("Rei");
+console.log(result);
+console.log("message 2: ", greeting("Freddie"));
+console.log("message 3: ", greeting("FrEddie").toLowerCase());
+console.log(`A message:  ${greeting("Freddie")}`);
+
+//exercise - Capitalize str
