eddae3ba9aa1bc7599ad0125eefd06a5682a4bc3
Rei Sikk <reix0010@stud.kea.dk>
2022-09-14 14:24:38 +0200

    previous commit did NOT work, working solution done(which is confusing to me)

--- leapyear.js
+++ leapyear.js
@@ -1,16 +1,24 @@
 "use strict";
 
-let year = Math.floor(Math.random() * 2023);
-let userInput = year;
-
+/* let year = Math.floor(Math.random() * 2023); */
+let year = 1900;
+/* let userInput = year; */
 console.log(year);
-console.log(userInput);
 
-isLeapYear();
-
+isLeapYear(year);
 function isLeapYear(year) {
-  function check100(year) {
-    if (userInput % 100 === 0) {
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isLeapYear(year));
+/* 
+function isLeapYear() {
+  function check100(userInput) {
+    console.log("check100 called");
+    if (userInput % 100 == 0) {
       return true;
     } else {
       return false;
@@ -23,19 +31,30 @@ function isLeapYear(year) {
     check4(userInput);
   }
   function check400(userInput) {
-    /*  console.log("check 400 called"); */
-    if (userInput % 400 === 0) {
-      console.log(`${userInput} is a leapyear!!!`);
+    console.log("check 400 called");
+    if (userInput % 400 == 0) {
+      return true;
     } else {
-      console.log(`${userInput} is not a leapyear`);
+      return false;
     }
   }
   function check4(userInput) {
     console.log("check 4 called");
-    if (userInput % 4 === 0) {
-      console.log(`${userInput} is a leapyear!!!`);
+    if (userInput % 4 == 0) {
+      return true;
     } else {
-      console.log(`${userInput} is not a leapyear`);
+      return false;
     }
   }
+  if (check100() === true && check4() === true) {
+    return true;
+  }
+  if (check100() === true && check4() === false) {
+    return false;
+  }
+  if (check100() === false && check400() === false) {
+    return false;
+  }
 }
+
+console.log(isLeapYear()); */
